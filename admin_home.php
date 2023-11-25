3a735871d836db6bea252ea86db131e864f7c994
amsyarkissmi <126231338+amsyarkissmi@users.noreply.github.com>
2023-11-25 15:32:27 +0800

    Add files via upload

---
+++ admin_home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Admin Home</title>
+</head>
+<body>
+<div class="admin-menu">
+    <h1>Welcome Admin !</h1>
+    <ul>
+        <li><a href="#">View Products</a></li>
+        <li><a href="user_management.php">User Management</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+</div>
+</body>
+</html>
